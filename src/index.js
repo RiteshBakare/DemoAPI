408ab3fe7c23fae0502ecdbf9b4bd12c615d8b1f
Ritesh Bakare <99970218+RiteshBakare@users.noreply.github.com>
2023-11-08 19:03:48 +0530

    Demo my route is Added to Project

--- src/index.js
+++ src/index.js
@@ -26,6 +26,9 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log(error);
 });
 
+app.get("/my",(req,res)=> {
+    res.send("Hello World :-) ")
+})
 
 // -------------------------------------------------------------------------------
 
